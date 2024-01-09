ea9c698a0abe44f5ad3d9c6819d04331111a3df3
GABRIEL CANIZA <bielcaniza6@gmail.com>
2024-01-09 14:35:01 -0300

    VERY BAD SOLUTION

---
+++ Tratamento de Excessões/Criando Excessões Personalizadas/Aula/src/model/entities/reservation.java
@@ -0,0 +1,64 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class reservation {
+    private Integer roomNumber;
+    private Date chekIn;
+    private Date chekOut;
+
+
+    //formatação de datas
+    private static SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
+
+    public reservation(Integer roomNumber, Date chekIn, Date chekOut) {
+        this.roomNumber = roomNumber;
+        this.chekIn = chekIn;
+        this.chekOut = chekOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getChekIn() {
+        return chekIn;
+    }
+
+    public Date getChekOut() {
+        return chekOut;
+    }
+
+    public long duration() {
+        long diff = chekOut.getTime() - chekIn.getTime();
+
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date chekIn, Date chekOut) {
+        this.chekIn = chekIn;
+        this.chekOut=chekOut;
+    }
+
+
+@Override
+public String toString(){
+    return "room"+
+    roomNumber+
+    ", check-in: "
+    + sdf.format(chekIn)+
+    ", check-out"
+    + sdf.format(chekOut)
+    +", "
+    +duration()
+    + "nights";
+}
+
+
+}
---
+++ Tratamento de Excessões/primeira solução(Muito ruim)/Aula/src/App.java
@@ -0,0 +1,49 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.reservation;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Room number: ");
+        int number = sc.nextInt();
+        System.out.println("Check-in date(dd/MM/yyyy)");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.println("Check-out date(dd/MM/yyyy)");
+        Date checkOut = sdf.parse(sc.next());
+
+        if (!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation: Check-out date mus be after check=in date");
+
+        } else {
+
+            reservation reservation = new reservation(number, checkIn, checkOut);
+            System.err.println("reservation: " + reservation);
+            System.out.println();
+            System.out.print("Enther data to update the reservation:");
+            System.out.println("Check-in date(dd/MM/yyyy)");
+            checkIn = sdf.parse(sc.next());
+            System.out.println("Check-out date(dd/MM/yyyy)");
+            checkOut = sdf.parse(sc.next());
+
+            Date now = new Date();
+            if (checkIn.before(now) || checkOut.before(now)) {
+                System.out.println("Error in reservation: Reservation dates for update must be future dates ");
+
+            } else if (!checkOut.after(checkIn)) {
+                System.out.println("Error in reservation: Check-out date mus be after check=in date");
+            }
+
+            else {// metódo utilizado pra Atualizar as datas
+                reservation.updateDates(checkIn, checkOut);
+                System.err.println("reservation: " + reservation);
+            }
+        }
+
+        sc.close();
+    }
+}
