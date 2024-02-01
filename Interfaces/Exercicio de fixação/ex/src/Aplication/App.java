4ec7b66b7b3d46a1c2dab4809012c9bb73a63832
GABRIEL CANIZA <bielcaniza6@gmail.com>
2024-02-01 07:36:13 -0300

    update curs

---
+++ Interfaces/Exercicio de fixação/ex/src/Aplication/App.java
@@ -0,0 +1,48 @@
+package Aplication;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+import Entities.Contract;
+import Entities.Installment;
+import services.ContractService;
+import services.PaypalService;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre com os dados do contrato");
+
+        System.out.print("numero: ");
+
+        int number = sc.nextInt();
+
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next().trim(), fmt);
+
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+
+        Contract obj = new Contract(number, date, totalValue);
+
+        System.out.print(" Entre com o numero de parcelas:");
+        int n = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+
+        contractService.processContract(obj, n);
+
+        System.out.println("Parcelas:");
+        for (Installment installment : obj.getInstallments()) {
+            System.out.println(installment);
+        }
+
+        sc.close();
+    }
+}
